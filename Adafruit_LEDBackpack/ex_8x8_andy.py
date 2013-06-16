522b477fc16f0c248190953ab28b3a9c7830e210
Andy Hull <andyhull3@gmail.com>
2013-06-16 11:23:15 -0700

    first blink test

--- Adafruit_LEDBackpack/ex_8x8_andy.py
+++ Adafruit_LEDBackpack/ex_8x8_andy.py
@@ -13,7 +13,7 @@ print "Press CTRL+Z to exit"
 
 iter = 0
 grid.setPixel(1, 1)
-blinkRate(2)
+grid.blinkRate(2)
 time.sleep(0.5)
 # Continually update the 8x8 display one pixel at a time
 # while(True):
